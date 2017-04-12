5f78e39b9f25ff35f8d3422fe991ed1bd9f9329d
lizihao <lizihaojlex@gmail.com>
2017-04-12 14:00:34 +0800

    //test

--- src/main/java/com/personal/control/UserController.java
+++ src/main/java/com/personal/control/UserController.java
@@ -3,7 +3,6 @@ package com.personal.control;
 
 import com.personal.entity.User;
 import com.personal.service.UserService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
